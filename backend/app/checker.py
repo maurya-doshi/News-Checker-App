81e38838b260fe49eb475ca55eb5afd39b065184
Mayura <mayurakrishna.25@proton.me>
2025-10-03 11:34:18 +0530

    Update checker.py to handle argument parsing

--- backend/app/checker.py
+++ backend/app/checker.py
@@ -3,9 +3,14 @@ from datetime import datetime
 import json
 import re
 import requests
+import time
 
 '''
     Class URL
+
+    Global variables:
+        1. s_time - Stores the start time of checking.
+        2. check_time - Stores the ISO time of checking.
 
     Instance variables:
         1. link - Stores the link that was passed.
@@ -26,11 +31,14 @@ import requests
 '''
 
 class URL:
-    def __init__(self, link)
+    s_time = time.time()
+    check_time = datetime.now()
+
+    def __init__(self, link):
         self.link = link
         self.is_true = False
         self.truth_percentage = 0.0
-        
+
     @property
     def link(self):
         return self._link
@@ -77,25 +85,40 @@ class URL:
     def truth_percentage(self, truth_percentage):
         # Ensure truth_percentage lies between 0 to 100.
         if not (0 <= truth_percentage <= 100):
-            raise ValueError("Default value is False!")
+            raise ValueError("Truth percentage can only lie in-between 0 to 100(inclusive)!")
 
         self._truth_percentage = truth_percentage
 
 
-    # Print the working url.
-    def __str__:
-        return f"Check at: {s_time}\nThe claim at: {self._url}, is most likely {self.is_true}.\nProbability of truth: {self.truth_percentage}.\n\nTime spent on analysis: {self._optime}"
+    # Print the status of check.
+    def __str__(self):
+        return f"Check at: {self.check_time}\nThe claim at: {self._link}, is most likely {self._is_true}.\nProbability of truth: {self._truth_percentage}.\n\nTime spent on analysis: {time.time()-self.s_time} s"
+
 
     def news_json(self):
         # TODO - send a get request using the requests module and print the output in JSON format.
         ...
 
-def main(argc, argv):
+
+def main():
+    # Command line arguments.
+    parser = argparse.ArgumentParser(prog = "checker.py", description = "A python program to probabilistically determine the truth value of a claim raised in a link on platforms like Reddit/X.", epilog = "Source @ github.com/maurya-doshi/News-Checker-App\nContributors:\n1.KaoKsn\n2.Maurya Doshi")
+
+    parser.add_argument("link") # Positional argument: link to the claim.
+
+    # Flags
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("-s", "--silent", action="store_true")
+    parser.add_argument("-V", "--version", action="store_true")
+
+    # Parsing the command line arguments.
+    args = parser.parse_args()
+
     # Create a news_engine object.
-    news_engine = URL(argv[1])
-    
+    news_engine = URL(args.link)
+
     # Read the contents of the link in JSON format.
-    news_engine.json()
+    news_engine.news_json()
 
     return 0;
 
