dd78b261e013a4d17dc7d5bf73ba2d698628abcd
Mayura <mayurakrishna.25@proton.me>
2025-10-03 16:15:00 +0530

    Update requirements and add a unit test template for checker.py

---
+++ backend/app/test/test_checker.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+def main():
+    # TODO: implement test functions that test the validity of different links provided as input.
+    ...
+
+
+if __name__ == "__main__":
+    main()
